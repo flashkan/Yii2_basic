654f42ac3b6bd0e9f4951e700c2817c3c17cafbe
Flashkan <nikol-babkin@yandex.ru>
2019-11-22 12:55:31 +0300

    init project

---
+++ views/site/some.php
@@ -0,0 +1,47 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Some text';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolore dolores eaque eius enim error facere impedit
+        incidunt ipsa ipsam iure magnam, obcaecati placeat praesentium quas qui quibusdam quidem quos ratione reiciendis
+        sit temporibus, voluptate? Assumenda at laborum minima vitae voluptatum! Aliquid consectetur consequatur cum,
+        delectus dicta eligendi enim et eum itaque molestiae neque perspiciatis provident, quis reprehenderit
+        repudiandae saepe sequi vero vitae. Ad aliquam asperiores consequuntur corporis, doloremque est incidunt iusto
+        laborum, maiores optio quae quam quo tenetur ut veniam? Ad aut beatae commodi consectetur consequatur, deserunt
+        enim et ex fugiat iusto minus mollitia nam nobis officia optio quaerat quasi qui quia quos rerum sequi ullam,
+        veritatis vitae. Aliquam esse minima reiciendis tempore velit. A autem culpa dolor doloribus error illum nemo,
+        nobis nulla officiis pariatur perferendis reiciendis repellat sit temporibus unde vel, voluptatem voluptatibus.
+        Adipisci aliquam animi asperiores assumenda atque consectetur consequatur cum cupiditate debitis distinctio
+        dolorum eaque enim, et eum exercitationem explicabo in incidunt ipsa iste iusto laudantium magnam maiores maxime
+        nihil non nulla, officia porro quam quis quisquam quod repellendus rerum saepe temporibus ut vitae voluptates?
+        Ad doloribus excepturi ipsa nostrum porro soluta vero voluptatum. Adipisci consectetur dolorum ex fugiat impedit
+        labore natus praesentium quas quis suscipit tempore tenetur, ullam vitae. Animi aspernatur dolorum eius error
+        esse facere fugiat labore libero nam nemo odit, perspiciatis praesentium quasi quia soluta tempore unde velit
+        veritatis voluptatem, voluptatibus. Ab adipisci architecto cupiditate, doloribus illum, itaque modi placeat qui
+        praesentium provident quia quod recusandae sequi sit tempore unde veniam voluptate. Accusantium animi, beatae
+        debitis deserunt iure mollitia perferendis? Amet aut consectetur dolor est, fuga impedit ipsum praesentium quae
+        quaerat repellendus? Adipisci aliquam, doloremque fugit, harum hic labore laborum magni nemo, nihil numquam
+        obcaecati repellat tempore velit? Dolorum error facere maiores, mollitia quasi quod veritatis voluptatibus?
+        Debitis eos eum illo non quaerat quia saepe similique tempore voluptatem voluptates? Consequatur cum ea ipsa
+        quos similique. Amet asperiores assumenda beatae blanditiis cupiditate delectus dolore hic, ipsam iste libero
+        maxime modi necessitatibus nostrum perspiciatis quae reprehenderit sequi tempore totam ullam voluptate! Debitis,
+        unde voluptas! Aliquid dolore, eos iste neque reprehenderit voluptatibus? Alias beatae deserunt distinctio.
+        Alias aut beatae debitis deleniti deserunt distinctio doloribus error, et eum hic maiores minus, molestiae nam
+        natus neque nobis numquam omnis optio pariatur perferendis recusandae saepe sapiente sit, tempora vel. Ea
+        laboriosam laudantium nam ullam. Amet asperiores atque beatae commodi consectetur corporis cupiditate dolor
+        dolorum, eius eum excepturi expedita itaque iure iusto labore natus non officia quae quaerat quidem quo saepe
+        sit tempore vitae voluptatibus? Accusantium adipisci at, debitis delectus, eum ipsam iste molestias officia quae
+        quibusdam quisquam recusandae repellendus ullam. Veniam.
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
